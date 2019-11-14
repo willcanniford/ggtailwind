0bc9b4e6c17f946c9bae1816d00c2954838c6ff8
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-14 11:58:25 +0000

    Add dataset with basic default palette splits

---
+++ R/data_2.R
@@ -0,0 +1,10 @@
+#' A collection of basic colour palettes 
+#'
+#' Combinations of colours found in \code{default_color_palette}. Split by base colour and shade. 
+#'
+#' @format A named list of length 20
+#' 
+#' @seealso \code{default_colour_palette}
+#' 
+"tailwind_palettes"
+
