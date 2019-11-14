48f4c39cecd6b93b736633bec4faba57b947abe9
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-14 12:44:48 +0000

    Create tailwind_pal function
    Generate palette functions based from palettes found in tailwind_palettes

---
+++ R/tailwind_pal.R
@@ -0,0 +1,21 @@
+#' Create palette function using tailwind_palettes
+#'
+#' This is a function to generate a palette function from \code{tailwind_palettes} for us with \code{discrete_scale} and \code{ggplot} functions. 
+#'
+#' @param palette Character name from \code{tailwind_palettes}
+#' @param reverse Boolean, whether you want to flip palette order
+#' @param ... Additional options to pass to \code{colorRampPalette}
+#' 
+#' @importFrom grDevices colorRampPalette
+#' 
+tailwind_pal <-
+function(palette = "main", reverse = FALSE, ...) {
+  basic_palettes <- get('tailwind_palettes')
+  selected_palette <- basic_palettes[[palette]]
+  
+  if (reverse) {
+    selected_palette <- rev(selected_palette)
+  }
+  
+  grDevices::colorRampPalette(selected_palette, ...)
+}
