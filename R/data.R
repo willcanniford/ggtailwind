b8a746923d5ba988c92129cd2d9acacb782fde4d
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-14 11:18:57 +0000

    Add documentation for the default colour palette

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Hex codes of all colours in tailwind css default colour palette
+#'
+#' A dataset containing 92 colours. 
+#'
+#' @format A named list of length 92. 
+#' 
+#' @source \url{https://tailwindcss.com/docs/customizing-colors/#default-color-palette}
+"default_colour_palette"
+
