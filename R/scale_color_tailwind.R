cb35c839bbd24c9a75082f5803e601e842500cb8
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-14 20:28:57 +0000

    Create scale_colour_tailwind
    Create function to handle the colour scales in ggplot2 using tailwind colour palettes

---
+++ R/scale_color_tailwind.R
@@ -0,0 +1,23 @@
+#' Use tailwind palettes to colour ggplot2 plots
+#'
+#' @param palette Character name from \code{tailwind_palettes}
+#' @param discrete Boolean, is the colour variable categorical/discrete
+#' @param reverse Boolean, whether you want to flip palette order
+#' @param ... Additional options to pass to \code{discrete_scale} or \code{scale_color_gradientn}
+#' 
+#' @importFrom ggplot2 scale_color_gradientn
+#' @importFrom ggplot2 discrete_scale
+#' 
+#' @export
+scale_color_tailwind <-
+function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  # Find the selected palette from tailwind_palettes
+  selected_palette <- tailwind_pal(palette = palette, reverse = reverse)
+  
+  # Pass to relevant function depending on whether the data is discrete
+  if (discrete) {
+    discrete_scale("colour", paste0("tailwind_", palette), palette = selected_palette, ...)
+  } else {
+    scale_color_gradientn(colours = selected_palette(256), ...)
+  }
+}
